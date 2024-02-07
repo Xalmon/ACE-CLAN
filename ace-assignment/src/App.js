e0f324cf1f0b25ebe63b001d4fddf61513f54792
Xalmon <xalmonoyefule@gmail.com>
2024-02-07 09:38:22 +0100

    [2]

---
+++ ace-assignment/src/App.js
@@ -0,0 +1,49 @@
+import logo from './logo.svg';
+import './App.css';
+import { Component } from 'react';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      aceClan : [
+        {
+          name: "Eddie"
+        },
+        {
+          name: "Joel"
+        },
+        {
+          name: "Laflare"
+        },
+        {
+          name: "Ashley"
+        }
+      ]
+    };
+  }
+
+
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Hello, I'm {this.state.name}.</h1>
+          <button onClick={() => {
+            this.setState({
+              name: "Baba"
+            }, () => {
+              console.log(this.state);
+            });
+          }}>
+            Change Name
+          </button>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
